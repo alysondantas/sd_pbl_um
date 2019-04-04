b09ccbcf94d1d568b7490d49eb5966e463d9f7d8
Marcos Vinicius <marcosviniciuscl@outlook.com>
2019-04-04 05:29:27 -0300

    Update algoritmo.s

--- assembly/algoritmo.s
+++ assembly/algoritmo.s
@@ -25,7 +25,7 @@ main:
 
 
 _lcd_init:
-	_delay_50ms								# aguarda a tensão estabilizar no display
+	_delay_50ms							# aguarda a tensão estabilizar no display
 	add r7, zero, 0x34						# 1 - linha, display on
 	add r8, zero, 0x1 						# comando enable do lcd
 	stwio r7, 0(r10)						# coloca o comando no data
@@ -44,25 +44,25 @@ _lcd_init:
 
 # ------------------------------------------- DELAY -----------------------------------------------
 _delay_200ms:
-    add r6, r6, 1       	   				# adiciona um ao contador
-    bne r6, CONT_200ms, _delay_200ms	    # continua chamando a label
-	add r6, zero, zero						# Zera o registrador r6, adiciona +20ns
-    ret  									# pc <- ra. Tempo da rotina 199,99996  milisegundos
+    add r6, r6, 1       	   					# adiciona um ao contador
+    bne r6, CONT_200ms, _delay_200ms	    				# continua chamando a label
+    add r6, zero, zero							# Zera o registrador r6, adiciona +20ns
+    ret  								# pc <- ra. Tempo da rotina 199,99996  milisegundos
 
 _delay_50ms:
-    add r6, r6, 1       	   				# adiciona um ao contador
-    bne r6, CONT_50ms, _delay_50ms	    	# continua chamando a label
-	add r6, zero, zero						# Zera o registrador r6, adiciona +20ns
-    ret  									# pc <- ra. Tempo da rotina 49,99996 milisegundos
+    add r6, r6, 1       	   					# adiciona um ao contador
+    bne r6, CONT_50ms, _delay_50ms	    				# continua chamando a label
+    add r6, zero, zero							# Zera o registrador r6, adiciona +20ns
+    ret  								# pc <- ra. Tempo da rotina 49,99996 milisegundos
 
 _delay_50micseg:
-    add r6, r6, 1       	   				# adiciona um ao contador
-    bne r6, CONT_50mics, _delay_50micseg    # continua chamando a label
-	add r6, zero, zero						# Zera o registrador r6, adiciona +20ns
+    add r6, r6, 1       	   					# adiciona um ao contador
+    bne r6, CONT_50mics, _delay_50micseg    				# continua chamando a label
+    add r6, zero, zero							# Zera o registrador r6, adiciona +20ns
     ret  	
 
 _delay_30micseg:
-    add r6, r6, 1       	   				# adiciona um ao contador
-    bne r6, CONT_30mics, _delay_30micseg    # continua chamando a label
-	add r6, zero, zero						# Zera o registrador r6, adiciona +20ns
-    ret  									# pc <- ra. Tempo da rotina 30,04 microsegundos
+    add r6, r6, 1       	   					# adiciona um ao contador
+    bne r6, CONT_30mics, _delay_30micseg    				# continua chamando a label
+    add r6, zero, zero							# Zera o registrador r6, adiciona +20ns
+    ret  								# pc <- ra. Tempo da rotina 30,04 microsegundos
