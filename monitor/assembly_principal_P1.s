08661b21762f32ae533c00b0732156a7a35d68a2
Lucas Cardoso <thelucascardoso@gmail.com>
2019-04-22 20:02:15 -0300

    Otimização do arquivo Assembly
    Co-Authored-By: Alyson Dantas <alyson@alysondantas.com.br>
    Co-Authored-By: Marcos Vinicius <marcosviniciuscl@outlook.com>

--- monitor/assembly_principal_P1.s
+++ monitor/assembly_principal_P1.s
@@ -1,52 +1,26 @@
 #===================================================================
-#---------- BOTÕES ----------------------
-# Endereço Base - 0x5060
-# S1 - SELECIONAR - 0xE - offset 0
-# S2 - VOLTAR     - 0xD - offset 4
-# S3 - DIREITA    - 0xB - offset 8
-# S4 - ESQUERDA   - 0x7 - offset c
-# Obs: os valores mais a direita pode ser que tenha que inverter a orde: S1->S4 e assim vai
-#------------------------------------------
-#------------ ESTADOS ---------------------
-# ESTADO 1 - 0x1 - 
-# ESTADO 2 - 0x2 - 
-# ESTADO 3 - 0x3 - 
-# ESTADO 4 - 0x4 - 
-# ESTADO 5 - 0x5 - 
-# r6 	- Registrador para o calculo do delay
-# r16 	- Instruções LCD
-# r17 	- Pino RS LCD
-# r23 	- Retorno do Custom
 #
-# 
 #=======================================================================
-.data
-botoes:		.word 0x000E, 0x000D, 0x000B, 0x0007
 .text
-
 .global main
 
 main:
 	br _init
-	# br _EXIBE
 	br _MOVIMENT
 	
 _init:
 	movia r8, 0xCB735 # Setando o DELAY 200ms
 	movia r9, 0x32DCD # Setando o DELAY 50ms	
-	movia r10, 0x023f0
-	addi r21, zero, 0x000E
+	movia r10, 0x023f0 # Definindo o endereço de memória para armazenar os valores dos botões
+	addi r21, zero, 0x000E # Valores dos Botões
 	stwio r21, 0(r10)
-	addi r21, zero, 0x000D
+	addi r21, zero, 0x000D # Valores dos Botões
 	stwio r21, 4(r10)
-	addi r21, zero, 0x000B
+	addi r21, zero, 0x000B # Valores dos Botões
 	stwio r21, 8(r10)
-	addi r21, zero, 0x0007
+	addi r21, zero, 0x0007 # Valores dos Botões
 	stwio r21, 12(r10)
-	addi r11, zero, 0x5070	# Endereco base dos leds
 	addi r12, zero, 0x3010	# Endereco base dos botão
-	#addi r12, zero, 0x0040	# Endereco base das codificações dos estados pt1
-	addi r14, zero, 0x0030	# Endereco base das codificações dos botões
 	addi r15, zero, 0x0 	# Registrador de comparação de botão
 	addi r18, zero, 0x0001 	# Registrador de comparação de estados
 	addi r20, zero, 0x1	# Registrador que guarda o estado atual
@@ -76,7 +50,6 @@ _init:
 _MOVIMENT:
 	
 	ldwio r15, 0(r12)			# Pega estado do botao
-	#call _delay_200ms
 	ldwio r18, 8(r10)			# Pega o valor referência pro botão direita
 	beq r15, r18, _botaoMenuDir
 	ldwio r18, 12(r10)			# Pega o valor referência pro botão esquerda
@@ -94,7 +67,7 @@ _BACK:
 	
 
 _botaoMenuDir:
-	#calcular para escrever nova msg usando um rAlgo que guarde o estado da opção
+	#calcular para escrever nova msg usando o r20 que guarde o estado da opção
 	subi r22, r20, 0x1
 	beq r22, zero, _OPDOISD
 	subi r22, r20, 0x2
@@ -105,12 +78,10 @@ _botaoMenuDir:
 	beq r22, zero, _OPCINCOD
 	subi r22, r20, 0x5
 	beq r22, zero, _OPUMD
-	#exibe o titulo 
-	#br _MOVIMENT
-	# br end
+
 
 _botaoMenuEsq:
-	#calcular para escrever nova msg usando um rAlgo que guarde o estado da opção
+	#calcular para escrever nova msg usando o r20 que guarde o estado da opção
 	subi r22, r20, 0x1
 	beq r22, zero, _OPCINCOE
 	subi r22, r20, 0x2
@@ -121,24 +92,19 @@ _botaoMenuEsq:
 	beq r22, zero, _OPTRESE
 	subi r22, r20, 0x5
 	beq r22, zero, _OPQUATROE
-	#br _MOVIMENT
-	# br end
 
 _botaoMenuSelect:
-	#addi r23, zero, 0x1
-	#calcular para escrever nova msg usando um rAlgo que guarde o estado da opção
+	#calcular para escrever nova msg usando o r20 que guarde o estado da opção
 	addi r20, r20, 0xA
 	
 	br _EXIBE_NOVO
-	# br end
 
 _botaoMenuBack:
-	#addi r23, zero, 0x00
-	#calcular para escrever nova msg usando um rAlgo que guarde o estado da opção
+	#calcular para escrever nova msg usando o r20 que guarde o estado da opção
 	subi r20, r20, 0xA
 	br _EXIBE
-	# br end
 	
+# Altera o valor do r20 dependendo do sentido da movimentação	
 #---------- Opções direita -----------------
 
 _OPUMD:
@@ -202,22 +168,22 @@ _EXIBE:
 	custom 0, r23, r17, r16 
 	call _delay_50ms
 	addi r17, zero, 0x1 # Registrador rs
-	addi r16, zero, 0x4F # Registrador d - caracter para escrita
+	addi r16, zero, 0x4F # Registrador d - caracter para escrita O
 	custom 0, r23, r17, r16 
 	call _delay_50ms
-	addi r16, zero, 0x50 # Registrador d - caracter para escrita
+	addi r16, zero, 0x50 # Registrador d - caracter para escrita P
 	custom 0, r23, r17, r16 
 	call _delay_50ms
-	addi r16, zero, 0x43 # Registrador d - caracter para escrita
+	addi r16, zero, 0x43 # Registrador d - caracter para escrita C
 	custom 0, r23, r17, r16 
 	call _delay_50ms
-	addi r16, zero, 0x41 # Registrador d - caracter para escrita
+	addi r16, zero, 0x41 # Registrador d - caracter para escrita A
 	custom 0, r23, r17, r16 
 	call _delay_50ms
-	addi r16, zero, 0x4F # Registrador d - caracter para escrita
+	addi r16, zero, 0x4F # Registrador d - caracter para escrita O
 	custom 0, r23, r17, r16 
 	call _delay_50ms
-	addi r16, zero, 0x20 # Registrador d - caracter para escrita
+	addi r16, zero, 0x20 # Registrador d - caracter para escrita Espaço
 	custom 0, r23, r17, r16 
 	
 	call _delay_50ms
@@ -235,46 +201,46 @@ _EXIBE:
 	
 	
 _EXIBEA:
-	addi r16, zero, 0x41 # Registrador d - caracter para escrita
+	addi r16, zero, 0x41 # Registrador d - caracter para escrita A
 	custom 0, r23, r17, r16
 	call _delay_50ms
-	addi r16, zero, 0x20 # Registrador d - caracter para escrita
+	addi r16, zero, 0x20 # Registrador d - caracter para escrita Espaço
 	custom 0, r23, r17, r16
 	
 	br _MOVIMENT
 
 _EXIBEB:
-	addi r16, zero, 0x42 # Registrador d - caracter para escrita
+	addi r16, zero, 0x42 # Registrador d - caracter para escrita B
 	custom 0, r23, r17, r16
 	call _delay_50ms
-	addi r16, zero, 0x20 # Registrador d - caracter para escrita
+	addi r16, zero, 0x20 # Registrador d - caracter para escrita Espaço
 	custom 0, r23, r17, r16
 
 	br _MOVIMENT
 	
 _EXIBEC:
-	addi r16, zero, 0x43 # Registrador d - caracter para escrita
+	addi r16, zero, 0x43 # Registrador d - caracter para escrita C
 	custom 0, r23, r17, r16
 	call _delay_50ms
-	addi r16, zero, 0x20 # Registrador d - caracter para escrita
+	addi r16, zero, 0x20 # Registrador d - caracter para escrita Espaço
 	custom 0, r23, r17, r16
 
 	br _MOVIMENT
 
 _EXIBED:
-	addi r16, zero, 0x44 # Registrador d - caracter para escrita
+	addi r16, zero, 0x44 # Registrador d - caracter para escrita D
 	custom 0, r23, r17, r16
 	call _delay_50ms
-	addi r16, zero, 0x20 # Registrador d - caracter para escrita
+	addi r16, zero, 0x20 # Registrador d - caracter para escrita Espaço
 	custom 0, r23, r17, r16
 
 	br _MOVIMENT
 	
 _EXIBEE:
-	addi r16, zero, 0x45 # Registrador d - caracter para escrita
+	addi r16, zero, 0x45 # Registrador d - caracter para escrita E
 	custom 0, r23, r17, r16
 	call _delay_50ms
-	addi r16, zero, 0x20 # Registrador d - caracter para escrita
+	addi r16, zero, 0x20 # Registrador d - caracter para escrita Espaço
 	custom 0, r23, r17, r16
 
 	br _MOVIMENT
@@ -302,7 +268,7 @@ _EXIBE_NOVO:
 	addi r16, zero, 0x43 # C
 	custom 0, r23, r17, r16 
 	call _delay_50ms
-	addi r16, zero, 0x49 # T
+	addi r16, zero, 0x49 # I
 	custom 0, r23, r17, r16 
 	call _delay_50ms
 	addi r16, zero, 0x4F # O
